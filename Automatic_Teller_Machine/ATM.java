a8263e4ea02f503a692e63a3399dd1f5206d8859
YogeshwaranKannan <129070212+YogeshwaranKannan@users.noreply.github.com>
2023-04-01 23:41:53 +0530

    Add files via upload

---
+++ Automatic_Teller_Machine/ATM.java
@@ -0,0 +1,13 @@
+package yogesh_first_project;
+
+import java.io.IOException;
+
+public class ATM extends Optionmenu {
+	public static void main(String[] args) throws IOException {
+		Optionmenu optionmenu = new Optionmenu();
+
+		optionmenu.getLogin();
+	}
+}
+
+
